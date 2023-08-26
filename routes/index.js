7c6cc179c9eccc16c092c371c85da67c1a85cfda
enCore_v1 <stepler028@gmail.com>
2023-08-26 17:15:25 +0300

    quotes

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ router.use('/users', usersRouter);
 router.use('/cards', cardsRouter);
 
 router.use((req, res) => {
-  res.status(ERROR_CODE_CAST).send({ message: "Запрашиваемый ресурс не найден" });
+  res.status(ERROR_CODE_CAST).send({ message: 'Запрашиваемый ресурс не найден' });
 });
 
 module.exports = router;
